3e79102203aa5925104756e0bfe83e11d7a77f4c
Nouhaila El Kante <nouhailaelkante2@gmail.com>
2026-03-28 11:35:57 +0100

    Refactor App component layout by adjusting indentation and structure for improved readability and organization.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,19 +7,19 @@ import { Footer } from "./components/footer";
 function App() {
   return (
     <Router>
-    <div className="flex flex-col min-h-screen w-full">
-      <Navbar />
+      <div className="flex flex-col min-h-screen w-full">
+        <Navbar />
 
-      <main className="flex-1 w-full bg-gray-100">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </main>
+        <main className="flex-1 w-full bg-gray-100">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </main>
 
-      <Footer />
-    </div>
-  </Router>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
